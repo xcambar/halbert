e7514eaf7bd29779442986581a5ac6fc66a66ac8
Xavier Cambar <xcambar@gmail.com>
2013-03-09 15:55:16 +0100

    Mandatory href property in Link

--- src/link.js
+++ src/link.js
@@ -1,8 +1,13 @@
 var utils = require('./utils');
 function Link(desc) {
+  "use strict";
   if (!utils.isObjectLiteral(desc)) {
     throw new TypeError('Invalid parameter passed to Link constructor');
+  }
+  if (!desc.hasOwnProperty('href')) {
+    throw new ReferenceError('Missing "href" property to link description');
   }
 }
 
 module.exports = Link;
+
--- test/specs/link_spec.js
+++ test/specs/link_spec.js
@@ -1,6 +1,12 @@
 /*global it:true, describe:true */
 var Link = require('src/link');
 
+function builder(arg) {
+  return function () {
+    return new Link(arg);
+  };
+}
+
 describe("Link", function () {
   "use strict";
   it("should be a function", function () {
@@ -8,13 +14,14 @@ describe("Link", function () {
   });
 
   it("MUST be an object literal", function () {
-    function builder(arg) {
-      return function () {
-        return new Link(arg);
-      };
-    }
-
     builder('str').should.throw(TypeError);
+  });
+
+  it("MUST have a HREF property", function () {
+    builder({}).should.throw(ReferenceError);
+    builder({}).should.throw(/Missing "href"/);
+
+    builder({href: '...'}).should.not.throw(Error);
   });
 });
 
